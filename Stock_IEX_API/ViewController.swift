e6650365f6846601b8bc1d778e1233d15fba600c
Shane Talbert <stalbert1@tampabay.rr.com>
2018-06-23 12:45:38 -0400

    Initial Commit

---
+++ Stock_IEX_API/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Stock_IEX_API
+//
+//  Created by Shane Talbert on 6/23/18.
+//  Copyright © 2018 Shane Talbert. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
